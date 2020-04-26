f576fdaa07ed24a9cd59008d06894dbe0ffa3836
deltaloose <takeshiroyu@gmail.com>
2020-04-26 23:26:39 +0900

    Nemasu

--- Bookers1/app/controllers/books_controller.rb
+++ Bookers1/app/controllers/books_controller.rb
@@ -6,8 +6,13 @@ class BooksController < ApplicationController
 	def create
 		@book = Book.new(book_params)
 		@book.user_id = current_user.id
-		@book.save
+		if @book.save
+		flash[:success] = 'Book was successfully create'
 		redirect_to book_path(@book.id)
+		else
+	    @books = Book.all
+	    render :index
+		end
 	end
 	def index
 		@book = Book.new
