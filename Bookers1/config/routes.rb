44ac3f0c7b58362bb22b94ac812f770c8f7328f4
deltaloose <takeshiroyu@gmail.com>
2020-05-10 14:29:21 +0900

    Favorite Finish

--- Bookers1/config/routes.rb
+++ Bookers1/config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   resources :users
-  resources :books
+  resources :books do
+  resource :favorites, only: [:create, :destroy]
+  resource :book_comments, only: [:create]
+  end
   get "home/about" => "homes#about"
 end
